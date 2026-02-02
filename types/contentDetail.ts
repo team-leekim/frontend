fd81c9af59bd1484821cfe965434c5dbf04257bd
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2026-02-02 11:38:03 +0900

    feat: api 호출 함수, 타입

---
+++ types/contentDetail.ts
@@ -0,0 +1,48 @@
+//뉴스 상세 정보
+
+export type ContentType = 'WEBTOON' | 'CARDNEWS';
+export type CategoryType =
+  | '정치'
+  | '경제'
+  | '사회'
+  | '국제'
+  | '문화'
+  | 'IT과학'
+  | '스포츠'
+  | '연예'
+  | '라이프'
+  | '기타';
+
+export type ReactionType = 'HAPPY' | 'SURPRISED' | 'SAD' | 'ANGRY' | 'EMPATHY';
+
+export interface ContentEditorDetail {
+  id: number;
+  name: string;
+  imageUrl: string;
+  keywords: string[];
+  description: string;
+}
+
+export interface OriginalArticle {
+  title: string;
+  url: string;
+  publishedAt: string;
+}
+
+export interface ContentDetailResponse {
+  id: number;
+  title: string;
+  category: CategoryType;
+  publishedAt: string;
+  contentType: ContentType;
+
+  summary: string[];
+  body: string;
+
+  editor: ContentEditorDetail;
+  imageUrls: string[];
+
+  reactionStats: Record<ReactionType, number>;
+
+  originalArticles: OriginalArticle[];
+}
