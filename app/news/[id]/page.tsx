4f8643e17ed3b38aaea637d946f49d363a2c7bce
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2026-02-02 17:13:46 +0900

    fix: page props type 에러

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -1,7 +1,14 @@
-// app/news/[id]/page.tsx
 import NewsDetailClient from '@/components/NewsDetailClient';
 import { newsDetailMock } from '@/mocks/newsDetail.mock';
 
-export default function NewsDetailPage({ params }: { params: { id: string } }) {
-  return <NewsDetailClient id={params.id} data={newsDetailMock} />;
+type PageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function NewsDetailPage({ params }: PageProps) {
+  const { id } = await params;
+
+  return <NewsDetailClient id={id} data={newsDetailMock} />;
 }
