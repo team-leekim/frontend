75ca2c34b78ca37d508d24f271dc2009ca3aa1c7
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2026-02-02 17:10:42 +0900

    fix: page props type error

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -2,12 +2,6 @@
 import NewsDetailClient from '@/components/NewsDetailClient';
 import { newsDetailMock } from '@/mocks/newsDetail.mock';
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-export default function NewsDetailPage({ params }: Props) {
+export default function NewsDetailPage({ params }: { params: { id: string } }) {
   return <NewsDetailClient id={params.id} data={newsDetailMock} />;
 }
